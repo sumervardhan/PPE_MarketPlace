0ceead6fbd012fa48cb6d5136b849f5247befeb0
Sumer Vardhan <sv110@duke.edu>
2020-04-12 22:12:34 +0530

    Connected to mongoDB database

--- server.js
+++ server.js
@@ -1,37 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 const apiKey = 'bd36cbc804584441c7c21b948094a79e';
+const connectionString = 'mongodb+srv://ppedata:samplepassword@cluster0-hpc2g.mongodb.net/test?';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-    res.render('index', {weather: null, error: null});
-})
+MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
 
-app.post('/', function (req, res) {
-    let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    const db = client.db('test')
 
-    request(url, function (err, response, body) {
-        if(err){
-            res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-            let weather = JSON.parse(body)
-            if(weather.main == undefined){
+    app.get('/', function (req, res) {
+        res.render('index', {weather: null, error: null});
+    })
+
+    app.post('/', function (req, res) {
+        let city = req.body.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+        request(url, function (err, response, body) {
+            if(err){
                 res.render('index', {weather: null, error: 'Error, please try again'});
             } else {
-                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-                res.render('index', {weather: weatherText, error: null});
+                let weather = JSON.parse(body)
+                if(weather.main == undefined){
+                    res.render('index', {weather: null, error: 'Error, please try again'});
+                } else {
+                    let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                    res.render('index', {weather: weatherText, error: null});
+                }
             }
-        }
-    });
+        });
+    })
+
+    app.listen(3000, function () {
+        console.log('PPE Marketplace listening on port 3000!')
+    })
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+
+
