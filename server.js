b29b312ffcc63bb3e0b56fb8272d2a49dce82402
Sumer Vardhan <sv110@duke.edu>
2020-04-15 16:14:58 +0530

    Integrated template into my app

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const request = require('request');
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-const apiKey = 'bd36cbc804584441c7c21b948094a79e';
+app.set('view engine', 'ejs')
+
 const connectionString = 'mongodb+srv://ppedata:samplepassword@cluster0-hpc2g.mongodb.net/test?';
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
 
 MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
 
@@ -19,7 +19,7 @@ MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) 
     const citiesDB = db.collection('cities')
 
     app.get('/', function (req, res) {
-        res.render('index', {weather: null, error: null});
+        res.render('templateIndex.ejs');
     })
 
     app.get('/search', function (req, res) {
